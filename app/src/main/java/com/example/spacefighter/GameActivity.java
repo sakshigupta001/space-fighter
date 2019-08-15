48b39a93c1e180c7d4b2a916be621fede3673977
Abhishek Kumar Gupta <abhishek.gupta1@rivigo.com>
2019-08-16 01:40:15 +0530

    added images and partial code for player, gam activity and game view

---
+++ app/src/main/java/com/example/spacefighter/GameActivity.java
@@ -0,0 +1,36 @@
+package com.example.spacefighter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GameActivity extends AppCompatActivity {
+
+    //declaring gameview
+    private GameView gameView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //Initializing game view object
+        gameView = new GameView(this);
+
+        //adding it to contentview
+        setContentView(gameView);
+    }
+
+    //pausing the game when activity is paused
+    @Override
+    protected void onPause() {
+        super.onPause();
+        gameView.pause();
+    }
+
+    //running the game when activity is resumed
+    @Override
+    protected void onResume() {
+        super.onResume();
+        gameView.resume();
+    }
+}
